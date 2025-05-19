062d2dea619225f618f7a3d080bf16cd7e9316f8
savag3kiwi <ryankidd@mmc.school.nz>
2025-05-20 08:54:03 +1200

    Updated string check v2
    It now Checks that users enter the full word or the 'n' letter/s of a word from a list of valid responses

---
+++ C_05_number_check_v2.py
@@ -0,0 +1,31 @@
+# Functions go here
+def string_check(question, valid_ans_list, num_letters):
+    """Checks that users enter the full word
+    or the 'n' letter/s of a word from a list of valid responses"""
+
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_ans_list:
+
+            # check if the response is the entire word
+            if response == item:
+                return item
+
+            # check if it's the 'n' letters
+            elif response == item[:num_letters]:
+                return item
+
+        print(f"Please choose an option from {valid_ans_list}")
+
+
+# Main routine goes here
+yes_no_list = ['yes', 'no']
+payment_list = ['cash', 'credit']
+
+like_coffee = string_check("Do you like coffee? ",
+                           yes_no_list, 1)
+print(f"You chose {like_coffee}")
+pay_method = string_check("Payment method: ", payment_list, 2)
+print(f"You chose {pay_method}")
